9f1088ddd2d135e1806c1b271afb2ddaad9f884f
高橋礼 <graphicocolo@gmail.com>
2026-02-22 11:52:41 +0900

    税込計算とBMI計算をUI切り替え

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -1,11 +1,23 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import TaxCalculator from '@/components/TaxCalculator'
+import BmiCalculator from '@/components/BmiCalculator'
 
 function App() {
+  const [showElement, setShowElement] = useState<'TaxCalculator' | 'BmiCalculator'>('TaxCalculator')
 
   return (
-    <div className="mx-auto max-w-2xl">
-      <TaxCalculator />
+    <div className="mx-auto max-w-2xl text-center">
+      <div className="mx-auto p-4 mt-4">
+        <select
+          className="ml-4 px-4 py-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-base"
+          onChange={(e) => setShowElement(e.target.value as 'TaxCalculator' | 'BmiCalculator')}
+          value={showElement}
+        >
+          <option value="TaxCalculator">税込価格計算</option>
+          <option value="BmiCalculator">BMI計算</option>
+        </select>
+      </div>
+      {showElement === 'TaxCalculator' ? <TaxCalculator /> : <BmiCalculator />}
     </div>
   )
 }
