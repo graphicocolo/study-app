49482770ff2a80ed239ede8bd8b36846035d835e
高橋礼 <graphicocolo@gmail.com>
2026-03-22 13:08:38 +0900

    配列データをDOMリスト表示

---
+++ udemy-js-app-study/05sort/script.js
@@ -0,0 +1,39 @@
+// HTML要素を取得
+/** @type {HTMLFormElement | null} */ 
+const submitForm = document.querySelector("#menuDisplayForm");
+/** @type {HTMLParagraphElement | null} */
+const element = document.querySelector("#menu");
+
+// メニューデータ
+const menuData = ['コーヒー', 'トースト', 'パフェ']
+
+/**
+ * データをリストで表示
+ * @param {Array} data 配列
+ * @param {HTMLElement} listElement DOM要素
+ * @returns {void}
+ */
+function listInsertData (data, listElement) {
+  if (data.length === 0) return;
+  for (let i = 0; i < data.length; i++) { // i 宣言には必ず let をつける
+    const listElementItem = document.createElement("li")
+    listElementItem.textContent = `「${data[i]}」`
+    listElement.appendChild(listElementItem)
+  }
+}
+
+// フォームが送信されたときの処理
+submitForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // フォームのデフォルトの送信を防止、ページ遷移を防止
+  if (element.hasChildNodes()) element.replaceChildren() // 連打対策
+  const menuList = document.createElement("ul") // HTML要素を生成
+  element.append(menuList) // 生成した要素を挿入
+  listInsertData(menuData, menuList)
+})
+
+// リセット処理
+submitForm.addEventListener("reset", () => {
+  // element.innerHTML = "" // HTMLパースが走る
+  // element.textContent = "" // HTMLパースが走らない、やや高速
+  element.replaceChildren()
+})
